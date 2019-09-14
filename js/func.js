22699b14dfd489d3c346bd75b698c7af6e453d1a
kirill <kirill07121@rambler.ru>
2019-09-14 14:07:09 +0300

    Готовый зелен

---
+++ js/func.js
@@ -0,0 +1,36 @@
+$(document).ready(function() { //Плавное открытие
+    $("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+
+  $("a.transition").click(function(event){
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(1000, redirectPage);
+  });
+
+  function redirectPage() {
+    window.location = linkLocation;
+  }
+});
+//Плавный скроллинг
+$(function(){
+$('a[href^="#"]').click(function(){
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+return false;
+});
+});
+
+//Проверка ответа
+function checkAnswers()
+{
+  var useranswer = document.getElementById("answer").value;
+  useranswer = useranswer.toLowerCase();
+  if (useranswer == "анастейша" || useranswer == "настя" || useranswer == "ананас" || useranswer == "анастасия")
+  {
+    alert("Умница, ты права");
+  }
+  else
+    alert("Вы явно ошибаетесь, сударыня");
+}
